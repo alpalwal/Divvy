f63e247a09e03d6691b944090ca69f0e036b581d
Alex Corstorphine <alex.corstorphine@divvycloud.com>
2019-07-25 10:41:38 -0400

    first push

---
+++ Prod/terraform/DivvyPowerUser.tf
@@ -0,0 +1,74 @@
+#Required for each account
+variable "external_id" {    
+  type = "string"
+  default = "divvycloud"
+}
+
+#Create the role and setup the trust policy
+resource "aws_iam_role" "divvycloud" {
+  name               = "DivvyCloud-cross-account"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::625820357955:root"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {
+        "StringEquals": {
+          "sts:ExternalId": "${var.external_id}"
+        }
+      }
+    }
+  ]
+}
+EOF
+}
+
+#Create the first readonly policy
+resource "aws_iam_policy" "readonly-policy1" {
+  name        = "divvycloud-readonly-policy1"
+  description = ""
+  policy      = "${file("read_only_policy1.json")}"
+}
+
+#Create the second readonly policy (it's too long to be in just 1 policy)
+resource "aws_iam_policy" "readonly-policy2" {
+  name        = "divvycloud-readonly-policy2"
+  description = ""
+  policy      = "${file("read_only_policy2.json")}"
+}
+
+#Create the write policy
+resource "aws_iam_policy" "power-user-policy" {
+  name        = "divvycloud-power-user-policy"
+  description = ""
+  policy      = "${file("power_user_policy.json")}"
+}
+
+#Attach 4 policies to the cross-account role
+resource "aws_iam_policy_attachment" "attach-diivvycloud-read-policy1" {
+  name       = "Attach-readonly1"
+  roles      = ["${aws_iam_role.divvycloud.name}"]
+  policy_arn = "${aws_iam_policy.readonly-policy1.arn}"
+}
+
+resource "aws_iam_policy_attachment" "attach-diivvycloud-read-policy2" {
+  name       = "Attach-readonly2"
+  roles      = ["${aws_iam_role.divvycloud.name}"]
+  policy_arn = "${aws_iam_policy.readonly-policy2.arn}"
+}
+
+resource "aws_iam_policy_attachment" "attach-diivvycloud-write-policy" {
+  name       = "Attach-write"
+  roles      = ["${aws_iam_role.divvycloud.name}"]
+  policy_arn = "${aws_iam_policy.power-user-policy.arn}"
+}
+
+#Output the role ARN
+output "Role_ARN" {
+  value = "${aws_iam_role.divvycloud.arn}"
+}
